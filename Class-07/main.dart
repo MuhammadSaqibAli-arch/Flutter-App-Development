a084491f1d392775364def204120d39139eff25a
MALIK-SAQIB <muhummadsaqibali@gmail.com>
2026-07-17 21:09:30 +0500

    Flutter Journey

---
+++ Class-07/main.dart
@@ -0,0 +1,18 @@
+void main() {
+    // while loop
+  int i = 1;
+
+  while (i <= 5) {
+    print("Number: $i");
+    i++;
+  }
+
+     // Do while Loop
+  int i = 1;
+
+  do {
+    print("Number: $i");
+    i++;
+  } while (i <= 5);
+
+}
