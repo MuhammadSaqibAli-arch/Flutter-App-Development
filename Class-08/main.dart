a9614dccced7bdd8b855928ee411440c588d8868
MALIK-SAQIB <muhummadsaqibali@gmail.com>
2026-07-18 21:05:21 +0500

    Flutter Journey

---
+++ Class-08/main.dart
@@ -0,0 +1,18 @@
+void main(){
+    function();
+    parameterFunction();
+}
+void function(){
+   int a = 12;
+   int b = 23;
+   int c = a +b;
+   print(c);
+
+}
+
+void parameterFunction(){
+    String first = "Muhammad Saqib";
+    String second = " Ali";
+    String fullName = first + second;
+    print(fullName);
+}
